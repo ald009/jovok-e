b98034d7d43eae9ea500e0a61eb37ae2b727af1f
ald009 <kovacs.tibor@diak.szbi-pg.hu>
2026-01-10 11:15:10 +0100

    asd

--- queries.sql
+++ queries.sql
@@ -11,3 +11,21 @@ select real_members, expected_members, real_members / expected_members as részv
 -- mennyi pénzt költött a csapat pályabérlésre az elmúlt 3 hónabban?
 select price, end_date from events where end_date >= timestampadd(month,-3, current_timestamp()) and end_date <= current_timestamp() ;
 -- EZT MEG SUMMAZNI KELL NEM NEHEZ NIGGER
+
+-- Nagy létszámú események
+SELECT event_name, expected_members FROM events WHERE expected_members > 12;
+
+-- Csapatok Budapetsről
+SELECT team_name, home_city FROM teams WHERE home_city = 'Budapest';
+
+-- Félprofi csapatok
+SELECT team_name, level FROM teams WHERE level = 'félprofi';
+
+-- Edzések listája
+SELECT event_name, start_date FROM events WHERE event_type = 'Edzés';
+
+-- Események maximális létszámmal
+SELECT event_name, max_participation FROM events;
+
+-- Játékosok névsorban
+SELECT name FROM players ORDER BY name ASC;
